e30ae18404022339ec7b388937b998bacaa083df
7Kaloyan7Markov7 <kamarkov@mail.bg>
2026-03-10 18:25:07 +0200

    Updated

--- scripts/entity.py
+++ scripts/entity.py
@@ -1,10 +1,21 @@
 from pygame import Vector2, Rect
 
 class Entity:
-    def __init__(self, entity_id, state, position, velocity, current_frame, hitbox):
+    def __init__(self, entity_id, state, position, velocity, current_frame_index, hitbox, sprites, direction):
+        self.direction = direction
+        self.sprites = sprites
         self.entity_id = str(entity_id)
         self.state = state
         self.position = Vector2(position)
         self.velocity = Vector2(velocity)
-        self.current_frame = int(current_frame)
-        self.hitbox = hitbox
+        self.current_frame_index = int(current_frame_index)
+        self.hitbox = Rect(hitbox)
+
+    def update():
+        pass
+
+    def load_assets():
+        pass
+
+    def render():
+        pass
