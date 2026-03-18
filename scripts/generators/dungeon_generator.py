e93aa94fe0fa5b169da2f9234946cd345c52d6b1
7Kaloyan7Markov7 <kamarkov@mail.bg>
2026-03-19 00:24:53 +0200

    fixed bugs

--- scripts/generators/dungeon_generator.py
+++ scripts/generators/dungeon_generator.py
@@ -9,17 +9,17 @@ class DungeonGenerator(Generator):
     def __init__(self, player_choice):
         self._spawner = Spawner()
         self._background_generator = BackgroundGenerator()
-        self._player = self.spawner.spawn_player(player_choice)
+        self._player = self._spawner.spawn_player(player_choice)
 
     @property
     def player(self):
         return self._player
 
     def generate_first_room(self):
-        background = self.background_generator.generate()
-        return Room(background, [], self.spawned_player)
+        background = self._background_generator.generate()
+        return Room(background, [], self._player)
 
     def generate(self):
-        spawned_entities = self.spawner.spawn_entities()
-        background = self.background_generator.generate()
+        spawned_entities = self._spawner.spawn_entities()
+        background = self._background_generator.generate()
         return Room(background, spawned_entities, None)
