e1b2b0d7a1fd82cf0a153739978c4896721868d8
7Kaloyan7Markov7 <kamarkov@mail.bg>
2026-04-13 23:52:43 +0300

    bfs added

--- scripts/tiles/tile_grid.py
+++ scripts/tiles/tile_grid.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from scripts.tiles.tile import Tile
 from scripts.entities.obstacle import Obstacle
 from scripts.enums.enums import Direction
@@ -32,6 +34,27 @@ class TileGrid:
                     if pos in self.tile_list:
                         tile.adjacent_tiles[direction] = self.tile_list[pos]
                         self.tile_list[pos].adjacent_tiles[opposites[direction]] = tile
+
+    def bfs(self, start_pos, end_pos):
+        if start_pos not in self.tile_list or end_pos not in self.tile_list:
+            return None
+
+        start = self.tile_list[start_pos]
+        end = self.tile_list[end_pos]
+
+        queue = deque([(start, [start])])
+        visited = {start}
+
+        while queue:
+            current, path = queue.popleft()
+            if current is end:
+                return path
+            for neighbor in current.adjacent_tiles.values():
+                if neighbor is not None and neighbor not in visited and not neighbor.is_blocked:
+                    visited.add(neighbor)
+                    queue.append((neighbor, path + [neighbor]))
+
+        return None
 
     # This method checks if any of the tiles in the grid are colliding with any of the entities
     def set_tile_is_blocking(self, entity_list):
